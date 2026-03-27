574c09368291277832b9572a82a5e7250d9e6aad
farheen2604 <farheen.learning264@gmail.com>
2026-03-27 14:20:24 +0000

    GCP Release Notes Agent — MCP Toolbox + BigQuery + ADK

---
+++ gcp_releasenotes_agent_app/agent.py
@@ -0,0 +1,18 @@
+from google.adk.agents import Agent
+from toolbox_core import ToolboxSyncClient
+
+toolbox = ToolboxSyncClient("http://127.0.0.1:5000")
+
+tools = toolbox.load_toolset('my_bq_toolset')
+
+root_agent = Agent(
+    name="gcp_releasenotes_agent",
+    model="gemini-2.5-flash",
+    description=(
+        "Agent to answer questions about Google Cloud Release notes."
+    ),
+    instruction=(
+        "You are a helpful agent who can answer user questions about the Google Cloud Release notes. Use the tools to answer the question"
+    ),
+    tools=tools,
+)
